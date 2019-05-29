801cdda41bf9b1f9c5d0fc8587e720c3cefe3328
Jamie McCrocklin <jamiemccrocklin@gmail.com>
2019-05-29 13:09:26 -0400

    added store.js

---
+++ store.js
@@ -0,0 +1,66 @@
+const STORE = [
+    {
+        question: 'Which of the following is NOT a leave-no-trace principle?',
+        answers: ['Respect wildlife', 'Pack out all trash', 'Bushwack when needed - some crags are difficult to find.', 
+        'Bring food, water, and essential climbing gear'],
+        correctAnswer: 'Bushwack when needed - some crags are difficult to find.'
+    },
+    {
+        question: 'What does the climbing etiquitte at the Red River Gorge say about cleaning?',
+        answers: ['Always lower.', 'Always rapel.', 'Lower or rapel, whichever you want.', 'There is no etiquette surrounding cleaning techniques.'],
+        correctAnswer: 'Always lower.'
+    },
+    {
+        question: 'Which of the following is poor etiquette?',
+        answers: ['Ticking all of the essential holds on the route and leaving them up to help future climbers send.', 
+        'Hanging hammocks out of the way of main hiking trails and belay spots.', 'Picking up trash, even if its not yours.',
+    'Letting other people work in if you have a large group of people on a route.'],
+        correctAnswer: 'Ticking all of the essential holds on the route and leaving them up to help future climbers send.'
+    },
+    {
+        question: 'What should you do if you arrive at a crag and it\'s overly crowded?',
+        answers: ['Climb there anyways.', 'Go to a different crag.', 'Heckle other climbers into leaving.',
+    'None of the above.'],
+        correctAnswer: 'Go to a different crag.'
+    },
+    {
+        question: 'Which of the following is NOT a reason why we respect local climbing ethics?',
+        answers: ['Climbing as a sport is exploding. This means more people at the crag, which means it is more important than ever before to protect the land we have.',
+    'We are not entitled to the areas we rock climb at. We can lose these privilages.', 'Good question... I don\'t know',
+'A lot of rules are in place to protect your safety.'],
+        correctAnswer: 'Good question... I don\'t know'
+    },
+    {
+        question: 'Which of the following is NOT true about bolting in the Red River Gorge?',
+        answers: ['Due to the rock type and humidity, zinc plated hardware is corroding.',
+    'Stainless steel glue-ins are being used to replace bolts across the U.S. and are a more sustainable option.',
+'The Red River Gorge Fixed Gear Initiative is working to replace corroded bolts and has already replaced over 1,000 bolts with donations.',
+'Stainless steel glue-ins are the best option because of how cheap they are.'],
+        correctAnswer: 'Stainless steel glue-ins are the best option because of how cheap they are.'
+    },
+    {
+        question: 'Which of the following statements about group sizing is NOT true?',
+        answers: ['Let other people work into climbs you are on.', 'Spread your group apart - not everyone should be in line for the same climb. Split up in pairs.',
+    'The philosophy on group sizing is: "The more, the merrier!"', 'Large groups can lead to a noisy and disruptive crag.'],
+        correctAnswer: 'The philosophy on group sizing is: "The more, the merrier!"'
+    },
+    {
+        question: 'Which of the following statements about trails is TRUE?',
+        answers: ['Always walk around mud - walking through just makes the problem worse.', 'Walk through the mud - walking around could lead to a wider trail.',
+    'It doesn\'t matter if you walk through the mud or around the mud.', 'None of the above.'],
+        correctAnswer: 'Walk through the mud - walking around could lead to a wider trail.'
+    },
+    {
+        question: 'Which of the following about human waste at the crag is FALSE?',
+        answers: ['As long as you are off the main walking trail, you are fine.', 'Pee at least 200 feet away from water sources.',
+    'Dig a hole with a shovel and burry the TP along with the waste or pack out all waste if possible.',
+'If there is an established bathroom, be sure to go before you climb or hike in.'],
+        correctAnswer: 'As long as you are off the main walking trail, you are fine.'
+    },
+    {
+        question: 'Which of the following statements is FALSE?',
+        answers: ['Carpool to save on fuel and parking spaces.', 'Keep the group size small.', 
+        'Take the most efficient route to the crag. Cut switchbacks.', 'Stay away from sensitive or eroded areas.'],
+        correctAnswer: 'Take the most efficient route to the crag. Cut switchbacks.'
+    },
+]
